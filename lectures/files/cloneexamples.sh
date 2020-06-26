fb7cc03aaefa61b7269cd3736e6f1ee4433184a1
fulanull <42985175+fulanull@users.noreply.github.com>
2020-06-26 19:02:51 -0300

    Set vim as default editor to git

--- lectures/files/cloneexamples.sh
+++ lectures/files/cloneexamples.sh
@@ -6,5 +6,5 @@ git clone git@github.com:jonasschmedtmann/complete-javascript-course.git
 #sets git config to be able to push to github
 git config --local user.email "42985175+fulanull@users.noreply.github.com"
 git config --local user.name "fulanull"
-
+git config --global core.editor "vim"
 
