244c2f16631d65e9ab4b75b31d4e1e7fb2684de2
fulanull <42985175+fulanull@users.noreply.github.com>
2020-07-07 01:00:45 -0300

    end of section 2

--- lectures/section2/main.js
+++ lectures/section2/main.js
@@ -132,7 +132,7 @@ var aTips = [];
 var aFinalAmount = [];
 
 
-for(i=0; i < aBills.length ; ++i )
+for(var i=0; i < aBills.length ; ++i )
 {
 	aTips[i] = billTax( aBills[i]);
 	aFinalAmount[i] = aTips[i] + aBills[i];
@@ -147,6 +147,157 @@ console.log("Total Amount:", aFinalAmount);
 //*/
 
 //Objects and properties  ###############################################
+//Challenge 4 ###############################################
+/*
+function getObject(pFullName, pMass, pHeight)
+{
+	var lObject =
+	{
+		fullName: pFullName,
+		mass: pMass,
+		height: pHeight,
+		calculateBMI: function ()
+		{
+			this.bmi = this.height*this.height;
+			this.bmi = this.mass/this.bmi;
+			return this.bmi;
+		},
+		compareBMI: function (person)
+		{
+			if( this.bmi === person.bmi)
+			{
+				console.log(this.fullName + " and " + person.fullName + " have the same BMI!");
+			}else if ( this.bmi >= person.bmi) {
+				console.log(this.fullName + " has greater BMI than " + person.fullName + "!");
+			}
+			else
+			{
+				console.log(person.fullName + " has greater BMI than " + this.fullName + "!");
+			}
+		},
+
+
+	};
+	lObject.calculateBMI();
+	return lObject;
+}
+
+
+var aux = getObject("Igor Menezes", 72, 1.79);
+var mark = getObject("Mark John", 70, 1.70);
+var john = getObject("John Mark", 70, 1.80);
+console.log(aux.fullName);
+console.log(typeof aux);
+console.log(aux);
+mark.compareBMI(john);
+
+//*/
+
+
+//Challenge 5 ###############################################
+
+var restaurantObj = function(pBillValue)
+{
+	var objTmp =
+	{
+		billValue: pBillValue,
+		calculateTotalAmount: function()
+			{
+				var tax = 0.1;
+				if(this.billValue < 50 )
+				{
+					tax = 0.2;
+				}
+				else if (this.billValue < 200) {
+					tax = 0.15;
+				}
+				this.calculatedTip = tax*this.billValue;
+				this.totalAmount = this.billValue + this.calculatedTip;
+			},
+		printTotal: function()
+			{
+				console.log(this.billValue + (" (value) ") + this.calculatedTip + " (tip) = " + this.totalAmount + " (Total Amount).");
+			},
+
+	};
+
+	objTmp.calculateTotalAmount();
+	return objTmp;
+}
+
+
+var billValuesArray = [124,48,268,180,42];
+var aRestaurants = [];
+var aTipsAverage = 0;
+for (var i=0; i < billValuesArray.length; ++i )
+{
+	aRestaurants.push( restaurantObj( billValuesArray[i]));
+	console.log("Restaurant", i+1)
+	aTipsAverage += aRestaurants[i].calculatedTip;
+	// aRestaurants[i].printTotal();
+}
+aTipsAverage /= i;
+// for(var i = 0; i < aRestaurants ; ++i)
+// {
+
+// }
+
+
+var restaurantMarkObj = function(pBillValue)
+{
+	var objTmp =
+	{
+		billValue: pBillValue,
+		calculateTotalAmount: function()
+			{
+				var tax = 0.25;
+				if(this.billValue < 100 )
+				{
+					tax = 0.2;
+				}
+				else if (this.billValue < 300) {
+					tax = 0.1;
+				}
+				this.calculatedTip = tax*this.billValue;
+				this.totalAmount = this.billValue + this.calculatedTip;
+			},
+		printTotal: function()
+			{
+				console.log(this.billValue + (" (value) ") + this.calculatedTip + " (tip) = " + this.totalAmount + " (Total Amount).");
+			},
+
+	};
+
+	objTmp.calculateTotalAmount();
+	return objTmp;
+}
+
+
+var billValuesArray = [77,375,110,45];
+var bRestaurants = [];
+var bTipsAverage = 0;
+for (var i=0; i < billValuesArray.length; ++i )
+{
+	bRestaurants.push( restaurantMarkObj( billValuesArray[i]));
+	console.log("Restaurant", i+1)
+	bTipsAverage += bRestaurants[i].calculatedTip;
+	// aRestaurants[i].printTotal();
+}
+bTipsAverage /= i;
+
+switch (true) {
+	case aTipsAverage  == bTipsAverage:
+		console.log("Same amount in tips");
+		break;
+		case aTipsAverage >= bTipsAverage:
+		console.log("A has paided the greater amount in tips");
+		break;
+	default:
+		console.log("B has paided the greater amount in tips");
+		break;
+}
+
+
 
 
 //*/
