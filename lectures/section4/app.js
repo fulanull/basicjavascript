6b18f53e4529f5f2236b614f7634569890129609
fulanull <42985175+fulanull@users.noreply.github.com>
2020-08-11 01:46:45 -0300

    end section 4

--- lectures/section4/app.js
+++ lectures/section4/app.js
@@ -12,7 +12,10 @@ GAME RULES:
 window.onload = function()
 {
     console.log('Loaded!');
-    document.querySelector(".dice").style.display = 'none';
+    dice_0_DOM = document.getElementById("dice-0");
+    dice_1_DOM = document.getElementById("dice-1");
+    dice_0_DOM.style.display = 'none';
+    dice_1_DOM.style.display = 'none';
     players = [ getPlayer("Igor", document.querySelector("#score-0") ),
                 getPlayer("Souza", document.querySelector("#score-1") ) ];
 
@@ -49,18 +52,42 @@ var players;
 var activePlayer ;
 var currentScore ;
 var pCurrent;
+var gamePlaying;
+var targetValue;
+var dice_0_DOM;
+var dice_1_DOM;
 
 
 
 //Reset
 function newGame()
 {
-    console.log('New clicked!!!');
+    // console.log('New clicked!!!');
     activePlayer = 0;
     currentScore = 0;
+    gamePlaying = true;
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+
+    var targetValueDOM = document.getElementById('targetPoint').value;
+
+    if( targetValueDOM>0 && targetValueDOM <=200)
+    {
+        targetValue = targetValueDOM;
+    }
+    else
+    {
+        targetValue = 20 ;
+    }
+
+    console.log('Target Value: ' + targetValue);
+
+    for (i in players)
+        document.querySelector('.player-' + i + '-panel').classList.remove('winner');
+    
     for (i in players )
     {        
         // players[i].debug();
+        document.getElementById('name-' + i ).textContent = players[i].name;
         players[i].reset();
     }
     otherPlayerTurn();
@@ -68,15 +95,9 @@ function newGame()
 }
 
 
-function refreshVariablesScreen()
-{
-
-
-}
-
 function updateCurrent()
 {
-    console.log('Active: ' + activePlayer);
+    // console.log('Active: ' + activePlayer);
     var currentMark = '#current-'+activePlayer;
     document.querySelector(currentMark).textContent = currentScore;
 
@@ -84,48 +105,81 @@ function updateCurrent()
 
 function hold()
 {
-    players[activePlayer].hold(currentScore);   
-    otherPlayerTurn();
+    if (gamePlaying )
+    {
+        players[activePlayer].hold(currentScore);   
+        otherPlayerTurn();        
+    }
 }
 
 function otherPlayerTurn()
 {   
     if( currentScore === 0 )
     {
-        players[activePlayer].reset();
+        //players[activePlayer].reset();
     }
-    updateCurrent();
-    activePlayer = (activePlayer + 1) % 2;
-    updateCurrent();
     currentScore = 0;
+    updateCurrent();
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    // document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+    activePlayer = (activePlayer + 1) % 2;
+    // document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+    currentScore = 0;
+    updateCurrent();
+}
+
+function winner()
+{
+    players[activePlayer].hold(currentScore);
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner');    
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');    
+    // console.log('Winner ' + activePlayer + '!!!!!!!1' );
+    document.getElementById('name-' + activePlayer ).textContent = 'Winner!';
+    //players[activePlayer].
 }
 
 function rollDice()
 {
-    //generate random number
-    document.querySelector(".dice").style.display = "";
-    var numberRolled = Math.floor( 6*Math.random()) + 1;
-    //update dice image
-    var imgName='dice-' + numberRolled + '.png';
-    var dice = document.querySelector(".dice");
-    dice.src = imgName;
-
-    if(numberRolled === 1 )
+    if (gamePlaying )
     {
-        currentScore = 0;
-        numberRolled = 0;
-        // console.log('Sorteou 1!!!');
-    }
-    currentScore += numberRolled;
-    updateCurrent();
+            //generate random number
+        dice_0_DOM.style.display = "";
+        var numberRolled0 = Math.floor( 6*Math.random()) + 1;
+        //update dice image
+        var imgName='dice-' + numberRolled0 + '.png';
+        dice_0_DOM.src = imgName;
 
-    if( currentScore === 0 )
-    {
-        otherPlayerTurn();
-    }
+        dice_1_DOM.style.display = "";
+        var numberRolled1 = Math.floor( 6*Math.random()) + 1;
+        //update dice image
+        var imgName='dice-' + numberRolled1 + '.png';
+        dice_1_DOM.src = imgName;
 
-    // console.log('-> ' + numberRolled + ' => ' + imgName);
-    // console.log(dice);
+        if(numberRolled0 === 1 || numberRolled1 === 1)
+        {
+            currentScore = 0;
+            numberRolled0 = 0;
+            numberRolled1 = 0;
+            // console.log('Sorteou 1!!!');
+        }
+        currentScore += numberRolled0 + numberRolled1;
+        updateCurrent();
+
+        if ( (players[activePlayer].score + currentScore) >= targetValue )
+        {
+            winner();
+            gamePlaying = false;
+        }
+
+        if( currentScore === 0 )
+        {
+            otherPlayerTurn();
+        }
+
+        // console.log('-> ' + numberRolled + ' => ' + imgName);
+        // console.log(dice);
+    }
 }
 
-
+//*/
