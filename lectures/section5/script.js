b72c5c63190411c6d89ca0a2bc127b953c34f0e1
fulanull <42985175+fulanull@users.noreply.github.com>
2020-08-11 02:38:27 -0300

    class 62 about objects

---
+++ lectures/section5/script.js
@@ -0,0 +1,28 @@
+//function constructor
+
+window.onload = function ()
+{
+    console.log('Loaded');
+    john.calculateAge();
+    jane.calculateAge();
+}
+
+var Person = function (name, yearOfBirth, job) { 
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    // this.calculateAge = function()
+    // {
+    //     console.log(2020 - this.yearOfBirth);
+    // }
+}
+
+Person.prototype.year = 2020;
+Person.prototype.calculateAge = function () {
+    console.log( this.name + ' has ' + (this.year - this.yearOfBirth) + ' years!');
+}
+
+
+var john = new Person('John', 1990, 'teacher');
+var jane= new Person('Jane', 1985, 'Secretary');
+
